5ce2b9fcb80cf8140ef44033bad4db1a9a265d3a
joseph <josephjunejoe@yahoo.com>
2021-06-03 01:28:06 +0800

    modular

---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const api = require('/api.js')
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the skem-api end-point")
+})
+
+app.post('/api/createTarget', api.createTarget)
+
+app.get('/api/getAllTargets', api.getAllTargets)
+
+app.post('/api/getOneTarget', api.getOneTarget)
+
+app.post('/api/updateTarget', api.updateTarget)
+
+app.post('/api/deleteTarget', api.deleteTarget)
+
+
+
